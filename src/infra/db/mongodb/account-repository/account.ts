9e220efa28f50324a29d4b2b02d5c40012a385a9
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-07-06 00:03:57 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+import { AddAccountReporitory } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountReporitory {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = result.ops[0]
+
+    const { _id, ...accountWithoutId } = account
+
+    return Object.assign({}, accountWithoutId, { id: _id })
+  }
+}
