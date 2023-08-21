e949f3b0b9017fa4ee8a92c432904c874af55431
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-08-21 00:16:34 -0300

    refactor: move required field validation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,6 +6,7 @@ import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -16,8 +17,9 @@ export const makeSignUpController = (): Controller => {
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoReporitory)
 
   const emailValidator = new EmailValidatorAdapter()
+  const validationComposite = makeSignUpValidation()
 
-  const signUpController = new SignUpController(emailValidator, dbAddAccount)
+  const signUpController = new SignUpController(emailValidator, validationComposite, dbAddAccount)
 
   const logMongoRepository = new LogMongoRepository()
 
---
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -0,0 +1,15 @@
+import { Validation } from './validation'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly valitions: Validation[]) {}
+
+  validate (input: any): Error {
+    for (const validation of this.valitions) {
+      const error = validation.validate(input)
+      if (error) {
+        return error
+      }
+    }
+    return null
+  }
+}
