1f552a91dff58bb591fa8c737d3d129730cb599a
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-09-06 19:29:49 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@presentation/protocols'
+import { LoginController } from '@presentation/controllers/login/login-controller'
+import { makeLoginValidation } from './login-validation-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '@main/factories/decorators/log-controller-decorator-factory'
+
+export const makeLoginController = (): Controller => {
+  const loginController = new LoginController(makeLoginValidation(), makeDbAuthentication())
+
+  return makeLogControllerDecorator(loginController)
+}
---
+++ src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@presentation/protocols'
+import { LogMongoRepository } from '@infra/db/mongodb/log/log-mongo-repository'
+import { LogControllerDecorator } from '@main/decorators/log-controller-decorator'
+
+export const makeLogControllerDecorator = (controller: Controller): Controller => {
+  const logMongoRepository = new LogMongoRepository()
+
+  return new LogControllerDecorator(controller, logMongoRepository)
+}
