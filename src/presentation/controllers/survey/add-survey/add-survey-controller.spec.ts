ec8d9b58e049a2b9236164f206f1642fba6906e4
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-09-08 13:51:09 -0300

    feat: ensure AddSurveyController returns a badRequest error if validations fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -43,4 +43,14 @@ describe('AddSurvey Controller', () => {
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse.statusCode).toEqual(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
 })
