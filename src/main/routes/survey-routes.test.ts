399ffd5ce46634bc00bd77d3c8fc9e94090219fc
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-09-12 10:02:51 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -73,5 +73,30 @@ describe('Survey Routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+
+    test('Should return 200 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Jose',
+        email: 'jose@mail.com',
+        password: '123'
+      })
+      const accessToken = sign({ id: res.insertedId.toHexString() }, env.jwtSecret)
+
+      await accountCollection.updateOne({ _id: res.insertedId }, { $set: { accessToken } })
+
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }])
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
