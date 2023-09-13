9c600af01421333641db5fe11bc5145e40bc327c
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-09-13 12:23:42 -0300

    feat: ensure save survey result route returns 403 if no token is provided

---
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -0,0 +1,12 @@
+import { SaveSurveyResultController } from '@presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
+import { Controller } from '@presentation/protocols'
+
+import { makeDbSaveSurveyResult } from '@main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory'
+import { makeLogControllerDecorator } from '@main/factories/decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveyById } from '@main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
+
+export const makeSaveSurveyResultController = (): Controller => {
+  const controller = new SaveSurveyResultController(makeDbLoadSurveyById(), makeDbSaveSurveyResult())
+
+  return makeLogControllerDecorator(controller)
+}
---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest'
+
+import { MongoHelper } from '@infra/db/mongodb/helpers/mongo-helper'
+import app from '@main/config/app'
+import env from '@main/config/env'
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.diconnect()
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
