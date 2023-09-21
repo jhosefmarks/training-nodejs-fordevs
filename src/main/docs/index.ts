64aa214c8ae3708b83fac3ba564608fbec7ad3d8
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-09-21 01:07:14 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Node API',
+    description: 'API de treinamento para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+}
