9aee32ea23fbbcd534557d7392ccc8dd6878d65f
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-09-22 22:29:40 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,27 @@
+import { mockLoadSurveyResultRepository } from '@data/test'
+
+import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+import { DbLoadSurveyResult } from './db-load-survey-result'
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+
+  return { sut, loadSurveyResultRepositoryStub }
+}
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('Should call LoadSurveyResultRepository', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+
+    await sut.load('any_survey_id')
+
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})
