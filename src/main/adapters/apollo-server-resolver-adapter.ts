96a0a9877ab80b0eb408b03103b0f9ac34b3ffb3
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-10-10 14:46:59 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,7 +1,12 @@
+import { GraphQLError } from 'graphql'
 import { Controller } from '@presentation/protocols'
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args)
 
-  return httpResponse.body
+  if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+    return httpResponse.body
+  } else {
+    throw new GraphQLError(httpResponse.body.message, { extensions: { code: httpResponse.statusCode } })
+  }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,9 +1,22 @@
 import { ApolloServer } from '@apollo/server'
+import { GraphQLError } from 'graphql'
 
 import resolvers from '@main/graphql/resolvers'
 import typeDefs from '@main/graphql/type-defs'
 
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    response.http.status = error.extensions.code
+  })
+}
+
 export const setupApolloServer = (): ApolloServer => new ApolloServer({
+  typeDefs,
   resolvers,
-  typeDefs
+  plugins: [{
+    requestDidStart: async () => ({
+      willSendResponse: async ({ response, errors }) => { handleErrors(response, errors) }
+    })
+  }]
 })
