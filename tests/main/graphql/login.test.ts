c16a80cd705478f968eabf65b968430bb09b525e
Jhosef Marks de Carvalho <jhosef@gmail.com>
2023-10-10 13:33:29 -0300

    feat: make login resolver use login controller

---
+++ tests/main/graphql/login.test.ts
@@ -0,0 +1,55 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { Express } from 'express'
+import { hash } from 'bcrypt'
+
+import { MongoHelper } from '@infra/db/mongodb'
+
+import { setupApp } from '@main/config/app'
+import env from '@main/config/env'
+
+let accountCollection: Collection
+let app: Express
+
+describe('Login GraphQL', () => {
+  beforeAll(async () => {
+    app = await setupApp()
+
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('Login Query', () => {
+    const query = `query {
+      login (email: "jose@mail.com", password: "123") {
+        accessToken
+        name
+      }
+    }`
+
+    test('Should return an Account on valid credentials', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Jose',
+        email: 'jose@mail.com',
+        password
+      })
+
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+
+      expect(res.status).toBe(200)
+      expect(res.body.data.login.accessToken).toBeTruthy()
+      expect(res.body.data.login.name).toBe('Jose')
+    })
+  })
+})
